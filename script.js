6c256faa93dc497809b8e9f1a079536075176fe9
Shivam0091 <kushwahashivam8699@gmail.com>
2023-12-28 11:57:51 +0530

    files added

---
+++ script.js
@@ -0,0 +1,127 @@
+var buttons = document.getElementById("buttons");
+var li = buttons.querySelectorAll("li");
+var result = document.getElementById("result");
+
+
+for (let i = 0; i < li.length; i++) {
+  li[i].addEventListener("click", function() {
+    if (this.innerHTML == "=") {
+      result.innerHTML = eval(result.innerHTML);
+    } else {
+      if (this.innerHTML == "÷") {
+        result.innerHTML += "/";
+      } else if (this.innerHTML == "x") {
+        result.innerHTML += "*";
+      } else if (this.innerHTML == 'CL') {
+        result.innerHTML = "";
+      } else {
+        result.innerHTML += this.innerHTML;
+      }
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
